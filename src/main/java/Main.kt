ebf874fc3eb1a83bd345f731b797bff7a1a5adb6
roborox <dev@roborox.org>
2020-04-07 22:24:33 +0300

    spring webflux setup

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,7 +1,16 @@
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Hello, world!")
+        val client = WebClient.builder().build()
+        val result = client.get()
+            .uri("https://google.com")
+            .retrieve()
+            .bodyToMono<String>()
+            .block()
+
+        println(result)
     }
 }
