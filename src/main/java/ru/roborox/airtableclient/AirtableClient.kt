3d45dae5d8d68fbe5562fb6b8da710664bfe4a7f
Roborox dev <dev@roborox.org>
2020-04-09 13:01:45 +0300

    var to val T parameter on AirtableClient

--- src/main/java/ru/roborox/airtableclient/AirtableClient.kt
+++ src/main/java/ru/roborox/airtableclient/AirtableClient.kt
@@ -1,43 +1,42 @@
 package ru.roborox.airtableclient
 
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.core.ResolvableType
 import org.springframework.http.MediaType
 import org.springframework.http.codec.json.Jackson2JsonDecoder
 import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
-import kotlin.properties.Delegates
 
-class AirtableClient(baseUrl: String) {
-    var client: WebClient by Delegates.notNull()
-
-    init {
+class AirtableClient<T>(
+    baseUrl: String,
+    clazz: Class<T>
+) {
+    private val pageType = ParameterizedTypeReference.forType<Page<T>>(ResolvableType.forClassWithGenerics(Page::class.java, clazz).type)
+    private val client: WebClient = {
         val objectMapper = ObjectMapper()
             .registerModule(KotlinModule())
             .registerModule(Jdk8Module())
             .registerModule(JavaTimeModule())
-
-        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
         val strategies = ExchangeStrategies
             .builder()
             .codecs {
                 it.defaultCodecs().jackson2JsonEncoder(Jackson2JsonEncoder(objectMapper, MediaType.APPLICATION_JSON))
                 it.defaultCodecs().jackson2JsonDecoder(Jackson2JsonDecoder(objectMapper, MediaType.APPLICATION_JSON))
             }.build()
-        client = WebClient.builder().exchangeStrategies(strategies).baseUrl(baseUrl).build()
-    }
+        WebClient.builder().exchangeStrategies(strategies).baseUrl(baseUrl).build()
+    }()
 
-    inline fun <reified T> getRecords(url: String, token: String): Mono<Page<T>> {
+    fun getRecords(url: String, token: String): Mono<Page<T>> {
         return client.get()
             .uri(url)
             .header("Authorization", token)
             .retrieve()
-            .bodyToMono()
+            .bodyToMono(pageType)
     }
 }
